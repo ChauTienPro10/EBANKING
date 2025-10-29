a8ce8792381dbac2444351efa3ccb3f1608ce73b
Thang Le <teddyle2003@gmail.com>
2025-10-29 09:31:01 +0700

    feat: initialize eKYC service with Spring Boot

---
+++ BACKEND/ekycService/src/test/java/com/ebanking/ekyc/EkycServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebanking.ekyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkycServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
