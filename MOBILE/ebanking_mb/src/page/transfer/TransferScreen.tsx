4f676f8395f69bf6957131e279689b0e24f3465b
Thang Le <teddyle2003@gmail.com>
2025-12-12 10:29:49 +0700

    Refactor code and config UI

--- MOBILE/ebanking_mb/src/page/transfer/TransferScreen.tsx
+++ MOBILE/ebanking_mb/src/page/transfer/TransferScreen.tsx
@@ -1,25 +1,15 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  Alert,
-  Platform,
-  Modal,
-  FlatList,
-} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { useNavigation } from '@react-navigation/native';
 import { useTranslation } from 'react-i18next';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { useSelector } from 'react-redux';
 import Colors from '../../constants/color';
 import CustomButton from '../../components/CustomButton';
 import { Header } from '../../components';
 import ConfirmTransferModal from '../../popups/ConfirmTransferModal';
 import EKYCRequiredModal from '../../components/EKYCRequiredModal';
-import PinRequiredModal from '../../components/PinRequiredModal';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../navigation/types';
 import { RootState, AppDispatch, store } from '../../store';
 import { fetchUserInfo } from '../../store/fetchAPI/UserInfoFetch';
@@ -47,6 +37,7 @@ import AmountInput from './components/AmountInput';
 import ContentInput from './components/ContentInput';
 import BankSelectionModal from './components/BankSelectionModal';
 import SavedAccountsModal from './components/SavedAccountsModal';
+import PinRequiredModal from '../../components/PinRequiredModal';
 
 type NavigationProp = NativeStackNavigationProp<RootStackParamList, 'Transfer'>;
 
@@ -220,9 +211,6 @@ const TransferScreen: React.FC<{ route: { params: TransferParams } }> = ({
         onCancel={handleCancelTransfer}
       />
 
-
-
-      {/* eKYC Required Modal */}
       <EKYCRequiredModal
         visible={showEKYCModal}
         onClose={() => setShowEKYCModal(false)}
@@ -257,11 +245,12 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   scrollContent: {
-    paddingBottom: 12,
-    paddingHorizontal: 16,
+    paddingBottom: 20,
+    paddingHorizontal: 20,
   },
   buttonContainer: {
-    marginTop: 8,
+    marginTop: 36,
+    marginBottom: 40,
   },
 });
 
