25c3f2f5738ef79be1e9a168418737ae346b3982
Thang Le <teddyle2003@gmail.com>
2025-12-14 11:41:18 +0700

    8-fix

--- MOBILE/ebanking_mb/src/page/card-info/components/index.ts
+++ MOBILE/ebanking_mb/src/page/card-info/components/index.ts
@@ -1,6 +1,5 @@
 export { CreditCard } from './CreditCard';
 export { CardLimitSection } from './CardLimitSection';
-export { TransactionList } from './TransactionList';
 export { EMVChip, ContactlessIcon } from './CardIcons';
 export { default as CardDetailBottomSheet } from './CardDetailBottomSheet';
 export { default as CardBalanceSection } from './CardBalanceSection';
